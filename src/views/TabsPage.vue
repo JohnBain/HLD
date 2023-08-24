48b6b1a4ae340a1d5bd3d01ddfb541d9ce5c424a
Amy <amy@MacBook-Pro.attlocal.net>
2023-08-23 20:21:55 -0700

    style

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -1,5 +1,6 @@
 <template>
   <ion-page>
+
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="top">
@@ -20,6 +21,9 @@
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
+
+  
+
   </ion-page>
 </template>
 
@@ -27,4 +31,9 @@
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { images, square, home, cash, menu } from 'ionicons/icons';
 import communitySvg from '/public/community.svg';
+
 </script>
+
+
+
+
